955028d0af63a62cb7f901a56d77e8e9eb1af239
FatAvocado <junpinchong@gmail.com>
2025-11-01 17:33:04 +0800

    Update communityPost.js

--- backend/routes/communityPost.js
+++ backend/routes/communityPost.js
@@ -26,7 +26,7 @@ const postSchema = Joi.object({
   culturalOrigin: Joi.string().max(100).required(),
   culturalStory: Joi.string().max(2000).required(),
   recipe: Joi.string().allow("", null),
-  userID: Joi.number().integer().required(),
+  userProfileID: Joi.number().integer().required(),
 });
 
 const commentSchema = Joi.object({
