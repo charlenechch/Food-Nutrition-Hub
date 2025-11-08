f24257a800aed0c1c37849ca753763cfbe7ab609
Sherman <666sherman666@gmail.com>
2025-11-08 22:05:57 +0800

    Update FoodDetailPage.jsx

--- frontend/src/pages/FoodDetailPage.jsx
+++ frontend/src/pages/FoodDetailPage.jsx
@@ -448,7 +448,7 @@ export default function FoodDetailPage() {
             {/* Health alerts */}
             {healthAlerts.length > 0 && (
               <div className="fdp-card">
-                <h3 className="fdp-section-title">
+                <h3 className="rdp-sec-title">
                   <TriangleAlert size={18} color={"#6a4a2f"} /> Health Information
                 </h3>
                 <div className="fdp-alerts">
