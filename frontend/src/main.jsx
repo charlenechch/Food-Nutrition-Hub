1c6d64ad259121855c6f0f6f063aaea4ea3504fb
charlenechch <102781873@students.swinburne.edu.my>
2025-11-20 10:37:18 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
-import "./i18n";
+import "./locales/i18n.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
