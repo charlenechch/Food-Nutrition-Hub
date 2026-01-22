71e58fa97ae6a6f1ffb5cf8409487cc4dd239ae3
JoeyTh04 <104384397@students.swinburne.edu.my>
2026-01-22 10:36:45 +0800

    .

--- frontend/src/pages/AdminFoodDatabase.jsx
+++ frontend/src/pages/AdminFoodDatabase.jsx
@@ -323,7 +323,7 @@ const AdminFoodDatabase = ({ categories = [] }) => {
       });
 
       // Send ALL data at once to bulk endpoint
-      const res = await fetch(`${API_URL}/api/foods/bulk-import`, {
+      const res = await fetch(`${API_URL}/api/foodSearch/bulk-import`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
