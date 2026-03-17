540ccebe74554bc3a7f08ef15aea5f16ccc15f6e
FatAvocado <junpinchong@gmail.com>
2026-03-17 22:33:11 +0800

    update

--- frontend/src/pages/AdminCommunityPostDatabase.jsx
+++ frontend/src/pages/AdminCommunityPostDatabase.jsx
@@ -225,7 +225,8 @@ const AdminCommunityPostDatabase = ({ posts: postsProp = [], sectionType = "appr
             )}
           </div>
           <button className="admin-recipe-btn-filter" onClick={() => setShowFilters(!showFilters)}>
-            <CiFilter className="filter-icon" /> {t("explore.filters")}
+            <CiFilter className="filter-icon" style={{ marginRight: "8px" }} /> 
+            {t("explore.filters")}
           </button>
         </div>
 
