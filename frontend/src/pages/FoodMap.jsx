f5624661924d9682230c2ba8c16f55fb95a1a692
charlenechch <102781873@students.swinburne.edu.my>
2026-03-24 18:15:11 +0800

    Fix 1

--- frontend/src/pages/FoodMap.jsx
+++ frontend/src/pages/FoodMap.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef, useCallback } from "react";
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "css/FoodMap.css";
+import "../css/FoodMap.css";
 
 const API = import.meta.env.VITE_API_URL || "https://api.sarawakeats.site";
 
