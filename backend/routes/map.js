20e4f3101a5140a8ed246846bc847353e204b8a5
charlenechch <102781873@students.swinburne.edu.my>
2026-03-24 14:14:11 +0800

    Update map.js

--- backend/routes/map.js
+++ backend/routes/map.js
@@ -5,7 +5,7 @@
 
 const express = require('express');
 const router  = express.Router();
-const { many } = require('../config/db');  // your existing MySQL pool
+const { many } = require('../config/db'); 
 const axios    = require('axios');
 
 const PLACES_KEY = process.env.GOOGLE_PLACES_KEY;
