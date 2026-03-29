fe07e0c20d1eaf25cb1a4d0672ec17ff01c15bc1
Sherman <666sherman666@gmail.com>
2026-03-29 14:43:19 +0800

    Update UserProfilePage.jsx

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -187,7 +187,7 @@ export default function UserProfilePage() {
   const { setBypassSessionCheck, user: authUser } = useAuth();
   //Controls view and edit mode
   const [isEditing, setIsEditing] = useState(false);
-  const [equippedBadge, setEquippedBadge] = useState("nutrition_scholar");
+  const [equippedBadge, setEquippedBadge] = useState("novice");
 
   //CSRF Token State
   const [csrfToken, setCsrfToken] = useState("");
