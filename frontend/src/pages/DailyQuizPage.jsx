d25a12fcde20c154c35854dcdad47c772f6e216e
Sherman <666sherman666@gmail.com>
2026-04-04 13:02:07 +0800

    bck

--- frontend/src/pages/DailyQuizPage.jsx
+++ frontend/src/pages/DailyQuizPage.jsx
@@ -73,7 +73,7 @@ export default function DailyQuizPage() {
         
         <button 
           onClick={() => navigate(-1)} 
-          className="lrp-btn lrp-btn-outline"
+          className="lrp-btn lrp-btn-outline dqp-back"
         >
           <span>&larr;</span> {t('quiz.back', 'Back')}
         </button>
