e5724b5d33a620b027b1e5269727fd18148b7283
Sherman <666sherman666@gmail.com>
2026-04-05 10:44:52 +0800

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -37,7 +37,7 @@ export default function Header() {
   const currentLang = i18n.language === "en" ? "EN" : "BM";
 
   const mockDailyQuiz = { 
-    lastCompletedDate: "2026-04-05", 
+    lastCompletedDate: "2026-04-04", 
     currentStreak: 40 
   };
 
