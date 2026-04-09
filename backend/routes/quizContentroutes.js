35374ac59834d8a44f952963f21c73142c9c9c0c
FatAvocado <junpinchong@gmail.com>
2026-04-10 02:45:10 +0800

    Revert "Update quizContentroutes.js"
    This reverts commit 1ca43065f95412fcfa0a4cbfb6d8ea796b889ca1.

--- backend/routes/quizContentroutes.js
+++ backend/routes/quizContentroutes.js
@@ -146,7 +146,6 @@ router.delete("/admin/:id", isAuthenticated, isAdmin, async (req, res) => {
     await db.execute(`DELETE FROM quiz_questions WHERE questionID = ?`, [req.params.id]);
     res.json({ success: true, message: "Deleted" });
   } catch (error) {
-    console.error("Admin delete error:", error);
     res.status(500).json({ error: "Failed delete" });
   }
 });
