f9aad8139afbd6988c0bd303c1e84daf5a862c41
FatAvocado <junpinchong@gmail.com>
2026-05-07 08:44:34 +0800

    Update likes.js

--- backend/routes/likes.js
+++ backend/routes/likes.js
@@ -26,10 +26,13 @@ const likeSchema = Joi.object({
 router.get("/post/:postId", async (req, res) => {
   try {
     const { postId } = req.params;
-    
-    // FIX APPLIED HERE: Changed u.username and u.profilePicture to up.username and up.profilePicture
+
     const [likes] = await db.execute(`
-      SELECT l.*, up.userProfileID, up.username, up.profilePicture 
+      SELECT 
+        l.*, 
+        up.userProfileID, 
+        CONCAT(u.firstname, ' ', u.lastname) AS username, 
+        up.avatar AS profilePicture 
       FROM likes l
       JOIN userProfile up ON l.userProfileID = up.userProfileID
       JOIN user u ON up.userID = u.userID
